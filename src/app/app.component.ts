491dddbce8eba962bc399126c6deca553f372879
seo <seogogle@gmail.com>
2020-10-07 11:45:58 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { EmailEditorComponent } from 'angular-email-editor';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'basic';
+}
